29a600ada6911835b6514dfb72d2ce2e97ea1a30
Ali Ranjbar <ali.ranjbar.lian@gmail.com>
2024-10-08 16:36:15 +0300

    Added reducer function & Error & Loader component

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,6 +1,9 @@
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
 import { useEffect, useReducer } from "react";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -17,7 +20,9 @@ function reducer(state, action) {
   }
 }
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -27,9 +32,13 @@ function App() {
   });
 
   return (
-    <div className="App">
+    <div className="app">
       <Header />
-      <Main></Main>
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+      </Main>
     </div>
   );
 }
