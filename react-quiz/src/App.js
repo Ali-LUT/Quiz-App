970cb333c1b35ef37f250864b7363ee25b4b2ee2
Ali Ranjbar <ali.ranjbar.lian@gmail.com>
2024-10-08 15:34:00 +0300

    Added json-server and questions data and fetched data from server

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,9 +1,19 @@
 import Header from "./Header";
+import Main from "./Main";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  });
+
   return (
     <div className="App">
       <Header />
+      <Main></Main>
     </div>
   );
 }
