0b283262a10ecbf8140559d2a767e9115ba3ec5a
Ali Ranjbar <ali.ranjbar.lian@gmail.com>
2024-10-08 21:15:13 +0300

    Added button functionality to show the question component

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -4,6 +4,7 @@ import Loader from "./Loader";
 import Error from "./Error";
 import { useEffect, useReducer } from "react";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -15,6 +16,8 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Action unknown");
   }
@@ -29,7 +32,7 @@ function App() {
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
-  });
+  }, []);
 
   return (
     <div className="app">
@@ -37,7 +40,10 @@ function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
